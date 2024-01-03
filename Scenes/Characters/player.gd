0aefbea021db8a74ee2a53aa9f2a91cbfd305d9b
Jessica Alder <jessalder1989@gmail.com>
2024-01-02 23:06:22 -0800

    Base character animations

--- Scenes/Characters/player.gd
+++ Scenes/Characters/player.gd
@@ -7,6 +7,9 @@ extends CharacterBody2D
 @export var gravity = 200
 @export var jump_force = 128
 
+@onready var animation_player = $AnimationPlayer
+@onready var sprite_2d = $Sprite2D
+
 const SPEED = 100.0
 const JUMP_VELOCITY = -200.0
 
@@ -18,6 +21,7 @@ func _physics_process(delta):
 	else:
 		apply_friction(delta)
 	jump()
+	update_animations(input_axis)
 	move_and_slide()
 
 
@@ -37,3 +41,16 @@ func apply_friction(delta):
 func jump():
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y += -jump_force
+
+func update_animations(input_axis):
+	if is_moving(input_axis):
+		animation_player.play("run")
+		sprite_2d.scale.x = sign(input_axis)
+	else:
+		animation_player.play("idle")
+	
+	if not is_on_floor():
+		if velocity.y < 0:
+			animation_player.play("jump")
+		elif velocity.y > 0:
+			animation_player.play("fall")
